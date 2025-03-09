4aafd35f746e17b945f1b31bd81a47830dd56995
Tanvir <zishantanvir05@gmail.com>
2025-03-09 17:46:41 +0600

    62.8

--- ema-john-pagination-server-starter/index.js
+++ ema-john-pagination-server-starter/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pmlso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +26,18 @@ async function run() {
         await client.connect();
 
         const productCollection = client.db("emaJohnDB").collection("products");
+
+        app.post('/productsByIds', async(req,res)=>{
+            const ids = req.body;
+            const idsWithObjectId = ids.map(id => new ObjectId(id))
+            const query = {
+                _id: {
+                    $in: idsWithObjectId
+                }
+            }
+            const result = await productCollection.find(query).toArray();
+            res.send(result)
+        })
 
         app.get("/products", async (req, res) => {
             const page = parseInt(req.query.page)
